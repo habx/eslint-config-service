e60ac85e51617a61df28825c7e3499e32e160af0
fabiencote <fabien.cote@me.com>
2021-04-26 14:02:28 +0200

    feat: remove no-shadow

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,79 +1,76 @@
 module.exports = {
-  'parser':'@typescript-eslint/parser',
-  'parserOptions':{
-    'ecmaVersion':2021,
-    'lib':['esnext']
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    lib: ['esnext'],
   },
-  'extends':[
-    'plugin:prettier/recommended'
-  ],
-  'plugins':[
-    '@typescript-eslint',
-    'import-helpers'
-  ],
-  'rules':{
-    'no-redeclare':'error',
-    'no-console':'error',
+  extends: ['plugin:prettier/recommended'],
+  plugins: ['@typescript-eslint', 'import-helpers'],
+  rules: {
+    'no-redeclare': 'error',
+    'no-console': 'error',
     'no-shadow': 'off',
-    'prefer-template':'error',
-    'no-empty':'error',
-    'no-debugger':'warn',
-    'no-unused-vars':'off',
+    'prefer-template': 'error',
+    'no-empty': 'error',
+    'no-debugger': 'warn',
+    'no-unused-vars': 'off',
 
-    'import-helpers/order-imports': ['warn', {
-      'newlinesBetween': 'always',
-      'groups': [
-        'module',
-        [
-          '/^@config/',
-          '/^@logger/',
-          '/^@pubSub/',
-          '/^@knex/',
-          '/^@links/',
-          '/^@drivers/',
-          '/^@typings/',
-          '/^@helpers/',
-          '/^@utils/',
-          '/^@middlewares/',
-          '/^@jobs/',
-          '/^@publishers/',
-          '/^@consumers/',
-          '/^@controllers/',
-          '/^@directives/',
-          '/^@schemas/',
-          '/^@graphql/',
-          '/^@resolvers/',
-          '/^@services/',
-          '/^@events/',
-          '/^@models/',
-          '/^@test/'
-        ],
-        'parent',
-        ['sibling', 'index']
-      ],
-      'alphabetize': { 'order': 'asc', 'ignoreCase': true }
-    }],
-
-    'prettier/prettier':[
+    'import-helpers/order-imports': [
       'warn',
       {
-        'trailingComma':'es5',
-        'tabWidth':2,
-        'semi':false,
-        'singleQuote':true,
-        'prefer-template':true
-      }
+        newlinesBetween: 'always',
+        groups: [
+          'module',
+          [
+            '/^@config/',
+            '/^@logger/',
+            '/^@pubSub/',
+            '/^@knex/',
+            '/^@links/',
+            '/^@drivers/',
+            '/^@typings/',
+            '/^@helpers/',
+            '/^@utils/',
+            '/^@middlewares/',
+            '/^@jobs/',
+            '/^@publishers/',
+            '/^@consumers/',
+            '/^@controllers/',
+            '/^@directives/',
+            '/^@schemas/',
+            '/^@graphql/',
+            '/^@resolvers/',
+            '/^@services/',
+            '/^@events/',
+            '/^@models/',
+            '/^@test/',
+          ],
+          'parent',
+          ['sibling', 'index'],
+        ],
+        alphabetize: { order: 'asc', ignoreCase: true },
+      },
     ],
-    '@typescript-eslint/no-shadow': 'error',
-    '@typescript-eslint/no-unused-vars':[
+
+    'prettier/prettier': [
+      'warn',
+      {
+        trailingComma: 'es5',
+        tabWidth: 2,
+        semi: false,
+        singleQuote: true,
+        'prefer-template': true,
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': [
       'error',
       {
-        'argsIgnorePattern':'^_',
-        'varsIgnorePattern':'^_'
-      }
-    ]
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
   },
-  'env':{
-    'node':true
-  }
+  env: {
+    node: true,
+  },
 }
